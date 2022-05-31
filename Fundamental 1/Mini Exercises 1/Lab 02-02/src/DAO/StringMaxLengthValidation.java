bfd766eba501cc4e4bd5fdca898e1e54264cb192
Nguyễn Đỗ Hà <53627312+avender199x@users.noreply.github.com>
2022-06-01 03:01:12 +0700

    Update StringMaxLengthValidation.java

--- Fundamental 1/Mini Exercises 1/Lab 02-02/src/DAO/StringMaxLengthValidation.java
+++ Fundamental 1/Mini Exercises 1/Lab 02-02/src/DAO/StringMaxLengthValidation.java
@@ -1,6 +1,7 @@
 package DAO;
 
 public class StringMaxLengthValidation implements IValidation{
+    private int Length;
     @Override
     public boolean Validate(String data) {
         return false;
