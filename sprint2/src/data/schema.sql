f1aeab9f8f84322f2a0c3a8a7400e6a8a687f60f
Nguyễn Đỗ Hà <53627312+avender199x@users.noreply.github.com>
2022-06-15 14:52:41 +0700

    Delete sprint2 directory

--- sprint2/src/data/schema.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE account (
-  Account_Number bigint NOT NULL,
-  Account_Type varchar(45) DEFAULT NULL,
-  Person_name varchar(50) DEFAULT NULL
-  );
--- sprint2/src/main/java/fis/criminal/model/enums/TrackAction.java
+++
@@ -1,7 +0,0 @@
-package fis.criminal.model.enums;
-
-public enum TrackAction {
-    SUBMITTED,
-    RETRIEVED,
-    RETURNED
-}
