92b3b5dc9c6db622a2d34b6cf8bfe3430ce75c01
Nguyen Do ha <103122202+DoHaNguyen@users.noreply.github.com>
2022-06-22 16:43:04 +0700

    Sprint3

---
+++ Sprint3/src/main/java/com/fresher/model/TrackEntry.java
@@ -0,0 +1,65 @@
+package com.fresher.model;
+
+import com.fresher.core.TrackAction;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+@Entity
+@Table(name = "Track_Entry")
+public class TrackEntry extends AbstractEntity {
+    protected LocalDateTime date;
+    @ManyToOne
+    @JoinColumn(name = "evidence_id")
+    private Evidence evidence;
+    @ManyToOne
+    @JoinColumn(name = "detective_id")
+    private Detective detective;
+    @Enumerated(EnumType.STRING)
+    private TrackAction action;
+
+    private String reason;
+
+    public TrackEntry() {
+        super();
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+
+    public Evidence getEvidence() {
+        return evidence;
+    }
+
+    public void setEvidence(Evidence evidence) {
+        this.evidence = evidence;
+    }
+
+    public Detective getDetective() {
+        return detective;
+    }
+
+    public void setDetective(Detective detective) {
+        this.detective = detective;
+    }
+
+    public TrackAction getAction() {
+        return action;
+    }
+
+    public void setAction(TrackAction action) {
+        this.action = action;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+}
