1f8ae3cce7a32355c7b9fd1f7a5fabc959aa46a7
Nguyen Do ha <103122202+DoHaNguyen@users.noreply.github.com>
2022-06-28 00:57:01 +0700

    Update DetectiveDto.java

--- Sprint_4/src/main/java/com/fis/Sprint_4/dto/DetectiveDto.java
+++ Sprint_4/src/main/java/com/fis/Sprint_4/dto/DetectiveDto.java
@@ -19,5 +19,5 @@ public class DetectiveDto extends AbstractEntityDto {
     private Boolean armed = false;
     @Enumerated(EnumType.STRING)
     private EmploymentStatus status = EmploymentStatus.ACTIVE;
-    private Set<Long> trackEntries;
+    private Set<CriminalCase> criminalCases;
 }
