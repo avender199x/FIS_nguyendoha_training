045a1eec18ae282dfade9c58e6a316afec9b3b98
Nguyen Do ha <103122202+DoHaNguyen@users.noreply.github.com>
2022-07-10 16:24:12 +0700

    Update UserController.java

--- blog/src/main/java/com/example/blog/controller/UserController.java
+++ blog/src/main/java/com/example/blog/controller/UserController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private UserService userService;
+    private final UserService userService;
 
     @Autowired
     public UserController(UserService userService) {
